92f5bc77bd7fefb37ff0c2d16ac3e6a654c0b9c5
Ondřej Vodáček <ondrej.vodacek@gmail.com>
2011-11-05 19:12:54 +0100

    fix comparison operator in validateRange

--- DateInput/DateInput.php
+++ DateInput/DateInput.php
@@ -191,12 +191,12 @@ class DateInput extends FormControl {
 	 */
 	public static function validateDateInputRange(self $control) {
 		if ($control->range['min'] !== null) {
-			if ($control->range['min'] >= $control->value) {
+			if ($control->range['min'] > $control->value) {
 				return false;
 			}
 		}
 		if ($control->range['max'] !== null) {
-			if ($control->range['max'] <= $control->value) {
+			if ($control->range['max'] < $control->value) {
 				return false;
 			}
 		}
