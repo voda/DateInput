b1f880e677d2a8aa2640044dae5cef408e1f33d0
Ondřej Vodáček <ondrej.vodacek@gmail.com>
2015-02-14 16:56:55 +0100

    travis: fix installing nette dev version

--- changeNetteVersion.sh
+++ changeNetteVersion.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i "s|\"nette/nette\": \"~2.0\"|\"nette/nette\": \"$1\"|" composer.json
+sed -i -e "s|\"nette/nette\": \"~2.0\"|\"nette/nette\": \"$1\"|" -e '1 a "minimum-stability": "dev",' composer.json
